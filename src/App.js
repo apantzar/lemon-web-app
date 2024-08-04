930a10b1b64a7fee5abde65cf05271f11642de74
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2024-08-04 22:02:41 +0300

    Footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Nav from "./components/Nav";
 import Main from "./components/Main";
 import { BrowserRouter as Router } from "react-router-dom";
 import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Nav />
         <Main />
         <Menu />
+        <Footer />
       </Router>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,67 @@
 import React from "react";
+import small_logo from "../assets/images/general/small_logo.png";
 
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 Restaurant</p>
+      <section>
+        <div className="company-info">
+          <img src={small_logo} alt="logo" />
+          <p>
+            Little Lemon <br />
+            Thessaloniki, Greece
+          </p>
+        </div>
+        <div>
+          <h3>Important Links</h3>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/services">Services</a>
+            </li>
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+              <a href="/reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="/orders">Orders online</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Contact</h3>
+          <ul>
+            <li>Phone: +30 2310 123456</li>
+            <li>Email: sample@mail.com </li>
+            <li>Address: address sample</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3>Social Media Links</h3>
+          <ul>
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <a href="#">Instagram</a>{" "}
+            </li>
+          </ul>
+        </div>
+      </section>
     </footer>
   );
 };
+
+export default Footer;
