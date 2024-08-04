783563a95af1c440a0a679af3433dd8cb773fc5e
Anastasios Pantzartzis <apantzar@csd.auth.gr>
2024-08-04 21:52:55 +0300

    Order confirmation added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,28 @@
 import React from "react";
 import recipes from "../constants/recipes";
+import Swal from "sweetalert2";
 
 const Menu = () => {
+  const handleOrder = (id) => {
+    console.log("Order placed for item with id: ", id);
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, order it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Ordered!",
+            text: "Your order has been processing.",
+            icon: "success"
+          });
+        }
+      });
+  };
   return (
     <div className="menu-container">
       <div className="menu-header">
@@ -19,7 +40,14 @@ const Menu = () => {
                 <p>{recipe.price}</p>
               </div>
               <p>{recipe.description}</p>
-              <button className="orderbtn">Order now</button>
+              <button
+                className="orderbtn"
+                onClick={() => {
+                  handleOrder(recipe.id);
+                }}
+              >
+                Order now
+              </button>
             </div>
           </div>
         ))}
